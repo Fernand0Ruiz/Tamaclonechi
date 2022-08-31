2def86ee38590e52b45c3eb08a7c46cf9858da61
Fernand0Ruiz <fern.adr.ruiz@gmail.com>
2022-08-30 20:48:13 -0700

    Added implementation to new repo.

---
+++ Tamaclonechi/src/Tamaclonechi.java
@@ -0,0 +1,15 @@
+/** 
+* FILE: Tamaclonechi.java
+* PURPOSE: The following program launches the GUI interface for the game
+* Tamaclonechi, by using an application launch of the class TamaclonechiGUI. 
+* 
+* @author Fernando Ruiz
+*/
+
+import javafx.application.Application;
+
+public class Tamaclonechi {
+	public static void main(String[] args) {
+		Application.launch(TamaclonechiGUI.class, args);
+	}
+}
